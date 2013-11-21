c0004d055cd5cf5197e77807ed633e7f155c7923
Ernandes Leite de Allmeida Guedes <endsalone@yahoo.com.br>
2013-11-21 11:10:40 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-//TRATAMENTO DE URL
-$_GET['url'] = (isset($_GET['url']) ? $_GET['url'] :'index');
-$rtrim = rtrim($_GET['url'], '/');
-$url = explode('/', $rtrim);
-$controller = $url[0];
-$method = $url[1];
-$param = $url[2];
+
 //ADIÇÃO DE ARQUIVOS MVC
+require 'libs/Bootstrap.php';
