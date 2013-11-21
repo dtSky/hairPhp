f348068e0fd046e1ce1f147a4f8ff82fe53e9d9f
Ernandes Leite de Allmeida Guedes <endsalone@yahoo.com.br>
2013-11-21 11:12:51 -0200

    Update Bootstrap.php

--- libs/Bootstrap.php
+++ libs/Bootstrap.php
@@ -1,2 +1,11 @@
 <?php
 
+//TRATAMENTO DE URL
+$_GET['url'] = (isset($_GET['url']) ? $_GET['url'] :'index');
+$rtrim = rtrim($_GET['url'], '/');
+$url = explode('/', $rtrim);
+$controller = $url[0];
+$method = $url[1];
+$param = $url[2];
+
+require 'controllers/'.$controller.'Controller.php';
